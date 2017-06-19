7b054aeff9498eff96bf7a374d41cce909ad9a6b
wbpcode <comems@msn.com>
2017-06-20 00:03:24 +0800

    complete snake game.

---
+++ SnakeGame/SnakeGame/GameEndWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakeGame
+{
+    /// <summary>
+    /// Interaction logic for GameEndWindow.xaml
+    /// </summary>
+    public partial class GameEndWindow : Window
+    {
+        MainWindow main;
+        public GameEndWindow(MainWindow w)
+        {
+            InitializeComponent();
+
+            main = w;
+            scoreTextBlock.Text = main.snake.length.ToString();
+            replayButton.Click += GameReplay;
+            endButton.Click += GameEnd;
+        }
+
+        private void GameEnd(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            main.Close();
+        }
+
+        private void GameReplay(object sender, RoutedEventArgs e)
+        {
+            main.timer.Stop();
+            main.snake = new Snake(main.mapCanvas);
+            this.Close();
+            main.timer.Start();
+        }
+    }
+}
--- SnakeGame/SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/SnakeGame/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace SnakeGame
     {
         public SnakeSection snakeFood;
         public Snake snake;
-        DispatcherTimer timer;
+        public DispatcherTimer timer;
         Random foodPoint;
 
         public MainWindow()
@@ -31,7 +31,7 @@ namespace SnakeGame
             InitializeComponent();
             foodPoint = new Random();
             snake = new Snake(mapCanvas);
-            snakeFood = new SnakeSection(foodPoint.Next(10,(int)mapCanvas.Width-10),foodPoint.Next(10,(int)mapCanvas.Height-10));
+            snakeFood = new SnakeSection(foodPoint.Next(10, (int)mapCanvas.Width - 10), foodPoint.Next(10, (int)mapCanvas.Height - 10));
             timer = new DispatcherTimer();
 
             foreach (var ss in snake.body)
@@ -42,16 +42,36 @@ namespace SnakeGame
 
             mapCanvas.MouseDown += MapCanvas_MouseDown;
 
-            timer.Interval = new TimeSpan(0,0,0,0,25);
+            replayMenuItem.Click += GameReplay;
+            pauseMenuItem.Click += GamePause;
+
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 25);
             timer.Tick += Timer_Tick;
             timer.Start();
         }
 
+        private void GameReplay(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            snake = new Snake(mapCanvas);
+            timer.Start();
+        }
+
+        private void GamePause(object sender, RoutedEventArgs e)
+        {
+            if (timer.IsEnabled) { timer.Stop(); } else { timer.Start(); }
+        }
+
+        private void GameEnd(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
         private void MapCanvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Point targetPosition=new Point();
+            Point targetPosition = new Point();
             targetPosition = e.GetPosition(this);
-            double temp= snake.speed / Math.Sqrt(
+            double temp = snake.speed / Math.Sqrt(
                 Math.Pow((targetPosition.X - snake.header.point.X), 2) +
                 Math.Pow((targetPosition.Y - snake.header.point.Y), 2));
             snake.orientation.X = temp * (targetPosition.X - snake.header.point.X);
@@ -61,10 +81,10 @@ namespace SnakeGame
         private void Timer_Tick(object sender, EventArgs e)
         {
             snake.SnakeUpdate();
-            if (snake.CheckDead(mapCanvas))
+            if (snake.CheckDead())
             {
                 timer.Stop();
-                GameEndWindow subWindow = new GameEndWindow();
+                GameEndWindow subWindow = new GameEndWindow(this);
                 subWindow.Owner = this;
                 subWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
                 subWindow.ShowDialog();
@@ -75,7 +95,7 @@ namespace SnakeGame
                 ViewUpdate(snakeFood);
             }
 
-            for (int i=0;i<snake.body.Count;i++)
+            for (int i = 0; i < snake.body.Count; i++)
             {
                 SnakeSection ss = snake.body[i];
                 mapCanvas.Children.Remove(ss.shape);
@@ -98,6 +118,8 @@ namespace SnakeGame
         public Point lastpoint = new Point();
         int stepNum = 0;
 
+        Canvas map;
+
         public int length { get { return body.Count; } }
         public double speed { get; set; }
 
@@ -112,6 +134,8 @@ namespace SnakeGame
             body.Add(header);
             lastpoint.X = body[body.Count - 1].point.X;
             lastpoint.Y = body[body.Count - 1].point.Y;
+
+            map = c;
         }
         public Snake(Canvas c, int s)
         {
@@ -124,6 +148,8 @@ namespace SnakeGame
             header.shape.Fill = Brushes.Blue;
             lastpoint.X = body[body.Count - 1].point.X;
             lastpoint.Y = body[body.Count - 1].point.Y;
+
+            map = c;
         }
 
         public void SnakeUpdate()
@@ -146,8 +172,6 @@ namespace SnakeGame
             header = temp;
 
             header.shape.Fill = Brushes.Blue;
-
-
         }
 
         public bool GetFood(SnakeSection f)
@@ -162,21 +186,19 @@ namespace SnakeGame
             return false;
         }
 
-        public bool CheckDead(Canvas map)
+        public bool CheckDead()
         {
-            if (header.point.X < 0 || header.point.X > map.Width||
+            if (header.point.X < 0 || header.point.X > map.Width ||
                 header.point.Y < 0 || header.point.Y > map.Height)
             {
                 return true;
             }
 
-            for(int i = 1; i < body.Count; i++)
+            for (int i = 1; i < body.Count; i++)
             {
                 if (Math.Sqrt(Math.Pow((header.point.X - body[i].point.X), 2) + Math.Pow((header.point.Y - body[i].point.Y), 2)) <= 10) { return true; }
             }
-
             return false;
-
         }
 
     }
@@ -196,7 +218,7 @@ namespace SnakeGame
             shape.Width = 10;
             shape.Fill = Brushes.Azure;
         }
-        public SnakeSection(double x,double y)
+        public SnakeSection(double x, double y)
         {
             point = new Point();
             shape = new Ellipse();
@@ -206,7 +228,7 @@ namespace SnakeGame
             shape.Width = 10;
             shape.Fill = Brushes.Azure;
         }
-        public SnakeSection(double x, double y,double w)
+        public SnakeSection(double x, double y, double w)
         {
             point = new Point();
             shape = new Ellipse();
@@ -216,7 +238,7 @@ namespace SnakeGame
             shape.Width = w;
             shape.Fill = Brushes.Azure;
         }
-        public SnakeSection(double x, double y, double w,SolidColorBrush c)
+        public SnakeSection(double x, double y, double w, SolidColorBrush c)
         {
             point = new Point();
             shape = new Ellipse();
